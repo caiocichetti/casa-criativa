7cab777306bc4c13fa85bd715e13b0b2a940dd63
Caio Cichetti <caio_cichetti@outlook.com>
2020-03-28 18:52:17 -0300

    Melhorando o front end

--- server.js
+++ server.js
@@ -4,51 +4,6 @@ const server = express();
 
 const db = require("./db");
 
-// const ideas = [
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-//         title: "Cursos de Programação",
-//         category: "Estudo",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-//         title: "Exercícios",
-//         category: "Saúde",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-//         title: "Meditação",
-//         category: "Mentalidade",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-//         title: "Karaokê",
-//         category: "Diversão em Família",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-//         title: "Pintura",
-//         category: "Criatividade",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-//     {
-//         img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-//         title: "Recortes",
-//         category: "Criatividade",
-//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-//         url: "http://rocketseat.com.br",
-//     },
-// ];
-
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"));
 
